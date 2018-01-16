cd647ff26cffa8bdb20cb21ee9b43c6b2c4ef5dd
lucas <d2194499406@gmail.com>
2018-01-16 16:15:53 +0800

    fix base function for add word and edit

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,36 @@
+class WordsController < ApplicationController
+
+  before_action :find_word, only: [:edit, :update]
+
+  def index
+    @words = Word.all
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    @word.save!
+    redirect_to words_path
+  end
+
+  def edit
+  end
+
+  def update
+    @word.update!(word_params)
+    redirect_to words_path
+  end
+
+  private
+
+  def find_word
+    @word = Word.find(params[:id])
+  end
+
+  def word_params
+    params.require(:word).permit(:spelling, :unit, :meaning, :word_type, :sentence, :synonym, :mistake)
+  end
+end
---
+++ db/migrate/20180116074904_create_words.rb
@@ -0,0 +1,15 @@
+class CreateWords < ActiveRecord::Migration[5.1]
+  def change
+    create_table :words do |t|
+      t.string :spelling
+      t.integer :unit
+      t.string :meaning
+      t.string :word_type
+      t.string :sentence
+      t.string :synonym
+      t.integer :mistake
+
+      t.timestamps
+    end
+  end
+end
