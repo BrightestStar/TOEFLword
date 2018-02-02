1948246e4c28df9eee8dfe18af3db7d074e468b4
lucas <d2194499406@gmail.com>
2018-02-02 17:01:31 +0800

    set unit input_status and fix unit min value for new word

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -7,13 +7,24 @@ class UnitsController < ApplicationController
   def create
     @unit = Unit.new(unit_params)
     @unit.save
-    
+
+    redirect_to '/'
+  end
+
+  def edit
+    @unit = Unit.find(params[:id])
+  end
+
+  def update
+    @unit = Unit.find(params[:id])
+    @unit.update!(unit_params)
+
     redirect_to '/'
   end
 
   private
 
   def unit_params
-    params.require(:unit).permit(:unit_number)
+    params.require(:unit).permit(:unit_number, :input_status)
   end
 end
